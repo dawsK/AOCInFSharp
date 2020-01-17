49332607724c3be0ecb46e920f12c809addb2674
Dawson Kroeker <dawsonk@iqmetrix.com>
2020-01-17 12:53:32 -0600

    AOC_1_1

---
+++ aoc_1.fsx
@@ -0,0 +1,21 @@
+open System
+
+type Mass = Mass of int
+
+type FuelRequired = FuelRequired of int
+
+let getFuelRequired (Mass mass): FuelRequired = FuelRequired((mass / 3) - 2)
+let getFuelInt (FuelRequired f) = f
+let readLines filePath = System.IO.File.ReadLines(filePath)
+
+let getFuelFromMass =
+    (int
+     >> Mass
+     >> getFuelRequired
+     >> getFuelInt)
+
+let writeResult filePath (result) = System.IO.File.WriteAllText(filePath, string result)
+
+readLines "C:\\code\\AdventOfCode\\aoc_input_1.txt"
+|> Seq.sumBy getFuelFromMass
+|> writeResult "C:\\code\\AdventOfCode\\aoc_output_1.txt"
